591c97fb6bac1b71dc2c8a0d19b31d6d1facf548
Greg Doud <gpdoud@gmail.com>
2020-08-26 08:37:46 -0400

    "final commit"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-ng-counter';
+  nbr: number = 0;
+  btnClass: string = '';
+
+  inc(): void { this.nbr++; this.display(); }
+  dec(): void { this.nbr--; this.display(); }
+
+  display(): void {
+    this.btnClass = '';
+    if(this.nbr % 2 == 0) {
+      this.btnClass += "text-danger ";
+    }
+    if(this.nbr % 3 == 0) {
+      this.btnClass += "font-weight-bold ";
+    }
+    if(this.nbr % 5 == 0) {
+      this.btnClass += "font-italic ";
+    }
+  }
 }
